eadf20ce5b73d71c837ea048f23405398c8a0ed1
DanilaRazinkov <danilarazinkov@gmail.com>
2020-10-05 22:19:04 +0300

    2

--- tree.cpp
+++ tree.cpp
@@ -5,79 +5,156 @@ using namespace std;
 
 Tree::Tree()
 {
-	head = NULL;
+    head = NULL;
 }
 
 void Tree::add(int x)
 {
-	add(x, 1, head);
+    add(x, head, NULL);
 }
 
-void Tree::add(int x, int level,  node* &now)
+void Tree::add(int x, node*& now, node* parent)
 {
-	if (now == NULL)
-	{
-		now = new(node);
-		now->value = x;
-		now->count = 1;
-		now->level = level;
-		now->right = NULL;
-		now->left = NULL;
-		if (level > max_level)
-			max_level = level;
-	}
-	else
-		if (now->value > x)
-			add(x, level + 1, now->right);
-		else
-			if (now->value < x)
-				add(x, level + 1, now->left);
-			else
-				now->count++;
+    if (now == NULL)
+    {
+        now = new(node);
+        now->value = x;
+        now->count = 1;
+        now->right = NULL;
+        now->left = NULL;
+        now->parent = parent;
+    }
+    else
+        if (now->value > x)
+            add(x, now->right, now);
+        else
+            if (now->value < x)
+                add(x, now->left, now);
+            else
+                now->count++;
 }
 
 
 Tree::node* Tree::found(int x)
 {
-	return found(x, head);
+    return found(x, head);
 }
 
 Tree::node* Tree::found(int x, node* now)
 {
-	if (now == NULL) return NULL;
-	else
-		if (now->value == x) return now;
-		else
-			if (x < now->value) return found(x, now->left);
-			else
-				if (x > now->value) return found(x, now->right);
+    if (now == NULL) return NULL;
+    else
+        if (now->value == x) return now;
+        else
+            if (x < now->value) return found(x, now->left);
+            else
+                if (x > now->value) return found(x, now->right);
 }
 
 void Tree::clean_tree()
 {
-	clean_tree(head);
+    clean_tree(head);
 }
 
 void Tree::clean_tree(node* now)
 {
-	if (now != NULL)
-	{
-		clean_tree(now->left);
-		clean_tree(now->right);
-		delete now;
-	}
+    if (now != NULL)
+    {
+        clean_tree(now->left);
+        clean_tree(now->right);
+        delete now;
+    }
+}
+
+void Tree::show_strat()
+{
+    show_strat(head);
+}
+
+void Tree::show_strat(node* now)
+{
+    if (now != NULL)
+    {
+        cout << now->value << ' ';
+        show_strat(now->left);
+        show_strat(now->right);
+    }
+}
+
+void Tree::show_back()
+{
+    show_back(head);
+}
+
+void Tree::show_back(node* now)
+{
+    if (now != NULL)
+    {
+        show_back(now->left);
+        show_back(now->right);
+        cout << now->value << ' ';
+    }
+}
+
+void Tree::show_sim()
+{
+    show_sim(head);
+}
+
+void Tree::show_sim(node* now)
+{
+    if (now != NULL)
+    {
+        show_sim(now->left);
+        cout << now->value << ' ';
+        show_sim(now->right);
+    }
+}
+
+void Tree::delete_el(node* now)
+{
+    if (now->right != NULL && now->left != NULL)
+    {
+        node* the_right = right(now->left);
+        now->value = the_right->value;
+        now->count = the_right->count;
+        if (the_right->left != NULL)
+        {
+            node* t = the_right;
+            the_right = the_right->left;
+            delete t;
+        }
+        else
+        {
+            node* t = the_right;
+            the_right = NULL;
+            delete t;
+        }
+
+    }
+}
+
+Tree::node* Tree::right(node* now)
+{
+    if (now != NULL)
+        if (now->right != NULL)
+            return right(now->right);
+        else
+            return now;
 }
 
 string Tree::tostring()
 {
-	if (head != NULL) return "{ " + to_string(head->value) + "(" + to_string(head->count) + "), left:{" + tostring(head->left) +
-		"}, right:{" + tostring(head->right) + "}";
-	else return "{-}";
+    if (head != NULL) return "{ " + to_string(head->value) + "(" +
+        to_string(head->count) + "), left:{" + tostring(head->left) +
+        "}, right:{" + tostring(head->right) + "}";
+    else return "{-}";
 }
 
 string Tree::tostring(node* now)
 {
-	if (now != NULL) return to_string(now->value) + "(" + to_string(now->count) + "), left:{" + tostring(now->left) +
-		"}, right:{" + tostring(now->right) + "}";
-	else return "-";
+    if (now != NULL) return to_string(now->value) + "(" +
+        to_string(now->count) + "), left:{" + tostring(now->left) +
+        "}, right:{" + tostring(now->right) + "}";
+    else return "-";
 }
--- tree.h
+++ tree.h
@@ -2,30 +2,38 @@
 
 class Tree
 {
-	struct node
-	{
-		int value;
-		int count;
-		int level;
-		node* right;
-		node* left;
-	};
-	struct list
-	{
-		node* value;
-		list* next;
-	};
-	node* head;
-	int max_level;
+    struct node
+    {
+        int value;
+        int count;
+        node* right;
+        node* left;
+        node* parent;
+    };
+    struct list
+    {
+        node* value;
+        list* next;
+    };
+    node* head;
 public:
-	Tree();
-	void add(int);
-	node* found(int);
-	node* found(int, node*);
-	void clean_tree();
-	void clean_tree(node*);
-	std::string tostring();
-	std::string tostring(node*);
+    Tree();
+    void add(int);
+    node* found(int);
+    void clean_tree();
+    void show_strat();
+    void show_back();
+    void show_sim();
+    void delete_el(node*);
+    std::string tostring();
 private:
-	void add(int, int, node*&);
+    void add(int, node*&, node*);
+    node* found(int, node*);
+    void clean_tree(node*);
+    void show_strat(node*);
+    void show_back(node*);
+    void show_sim(node*);
+    node* right(node*);
+    std::string tostring(node*);
 };
+
